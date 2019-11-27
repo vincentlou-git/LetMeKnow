035632135ebee5fcf31e2cdddd6ec4175c36c0bd
DoraMemo-x <doraxmemory@gmail.com>
2019-11-27 23:19:34 +0000

    Initial Commit

---
+++ LetMeKnow/LetMeKnow/RegisterCode.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LetMeKnow
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RegisterCode : ContentPage
+	{
+		public RegisterCode ()
+		{
+			InitializeComponent ();
+		}
+
+        private void email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            button.IsEnabled = email.Text != "";
+        }
+
+        private async void button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Check you mailbox!", "We have sent an confirmation email to " + email.Text + ".", "Got it!");
+        }
+    }
+}
