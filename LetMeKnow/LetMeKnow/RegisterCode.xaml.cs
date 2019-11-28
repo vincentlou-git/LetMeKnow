4fd0b0a4b589de08205ac31a545abd656a4e90a1
DoraMemo-x <doraxmemory@gmail.com>
2019-11-28 03:44:34 +0000

    Attempt of email registration
    TODO:
    Use the Xamarin Messaging plugin instead of relying on Xamarin.Essentials

--- LetMeKnow/LetMeKnow/RegisterCode.xaml.cs
+++ LetMeKnow/LetMeKnow/RegisterCode.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace LetMeKnow
 {
@@ -24,7 +25,31 @@ namespace LetMeKnow
 
         private async void button_Clicked(object sender, EventArgs e)
         {
+            await SendEmail("LetMeKnow Registration",
+                            "Hi,\n\nClick the link below to activate your LetMeKnow account!\n\nLINK",
+                            new List<string> { email.Text } );
             await DisplayAlert("Check you mailbox!", "We have sent an confirmation email to " + email.Text + ".", "Got it!");
         }
+
+        // ------- <Ref> Taken from https://docs.microsoft.com/en-us/xamarin/essentials/email?tabs=android --------
+        private async Task SendEmail(string subject, string body, List<string> recipients) {
+            try {
+                var message = new EmailMessage {
+                    Subject = subject,
+                    Body = body,
+                    To = recipients,
+                    //Cc = ccRecipients,
+                    //Bcc = bccRecipients
+                };
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException fbsEx) {
+                // Email is not supported on this device
+            }
+            catch (Exception ex) {
+                // Some other exception occurred
+            }
+        }
+        // ---------- </Ref> Taken -----------
     }
 }
