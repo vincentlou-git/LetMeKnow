574fba7774e0e5a338807bd51a2ed8faad5a4ca8
DoraMemo-x <doraxmemory@gmail.com>
2019-12-08 17:11:44 +0000

    Added PropertyChanged.Fody

--- LetMeKnow/ViewModels/ViewModel.cs
+++ LetMeKnow/ViewModels/ViewModel.cs
@@ -4,6 +4,7 @@ namespace LetMeKnow.ViewModels
 {
     // All ViewModel subclasses should extend this to have property changed detection
     // so targets can be updated when a property changes
+    // Added PropertyChanged.Fody (a helper nuget) that generates the code to handle property updates
     public class ViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
